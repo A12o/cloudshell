dec1bc628be20db1bfc4c5513322e129f69a1109
a12o <Arnold.Lasrado@gmail.com>
2020-01-24 02:10:59 -0500

    Feat(cloudshell): Install script zsh Cloudshell add .cloudshell as hidden folder

--- install.sh
+++ install.sh
@@ -10,21 +10,23 @@ brew install zsh
 brew install git vim jq tig fzf
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
-git clone https://github.com/A12o/cloudshell.git
+git clone https://github.com/A12o/cloudshell.git .cloudshell
 
 rm -rf ~/.vimrc ~/.tmux.conf
 
-ln -s ~/cloudshell/Dotfiles/.vimrc ~/.vimrc
-ln -s ~/cloudshell/Dotfiles/.zshrc ~/.zshrc
-ln -s ~/cloudshell/profile.d ~/profile.d
+ln -s ~/.cloudshell/Dotfiles/.vimrc ~/.vimrc
+ln -s ~/.cloudshell/Dotfiles/.zshrc ~/.zshrc
+ln -s ~/.cloudshell/profile.d ~/profile.d
 
 cat >> ~/.bashrc << BOF
-bash -c zsh
+zsh
 BOF
 
 brew install ruby
 gem install tmuxinator
-ln -s ~/cloudshell/Dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/cloudshell/Dotfiles/.tmuxinator ~/.tmuxinator
+ln -s ~/.cloudshell/Dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.cloudshell/Dotfiles/.tmuxinator ~/.tmuxinator
 
 echo -e "\n Restart Cloud Shell for changes to take effect"
+
+exit
