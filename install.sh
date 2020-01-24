3a18a42c528e54ff6160c9d094d9de6b1cc6134d
a12o <Arnold.Lasrado@gmail.com>
2020-01-24 00:52:01 -0500

    Feat(cloudshell): Install script zsh Cloudshell, tmuxinator

--- install.sh
+++ install.sh
@@ -3,15 +3,24 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
 brew update
-brew install zsh #zsh-completions
-brew install git vim jq
-git clone https://github.com/ohmyzsh/ohmyzsh.git
+brew install zsh
+brew install git vim jq tig
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
 git clone https://github.com/A12o/cloudshell.git
+
 rm -rf ~/.vimrc ~/.tmux.conf
+
 ln -s ~/cloudshell/Dotfiles/.vimrc ~/.vimrc
-ln -s ~/cloudshell/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/cloudshell/Dotfiles/.zshrc ~/.zshrc
 ln -s ~/cloudshell/profile.d ~/profile.d
+
 cat >> ~/.bashrc << BOF
 bash -c zsh
 BOF
+
+gem install tmuxinator
+ln -s ~/cloudshell/Dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/cloudshell/Dotfiles/.tmuxinator ~/.tmuxinator
+
+exit
