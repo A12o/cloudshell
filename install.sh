eded0aa03be2e263721e9cdecbf68e3d05b61310
a12o <Arnold.Lasrado@gmail.com>
2020-01-31 14:24:45 -0500

    Feat(install): update

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ brew update
 brew install zsh
 brew install vim jq tig fzf git httpie
 
+rm -rf ~/.vimrc ~/.tmux.conf ~/.tmuxinator ~/.zshrc ~/profile.d ~/.ohmyzsh ~/.cloudshell
+
 git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
 git clone https://github.com/A12o/cloudshell.git .cloudshell
-
-rm -rf ~/.vimrc ~/.tmux.conf ~/.tmuxinator ~/.zshrc ~/profile.d
 
 ln -s ~/.cloudshell/Dotfiles/.vimrc ~/.vimrc
 ln -s ~/.cloudshell/Dotfiles/.zshrc ~/.zshrc
@@ -24,6 +24,7 @@ cp -pR $HOME/linuxbrew /home/linuxbrew
 bash -c zsh
 BOF
 
+rm -rf $HOME/linuxbrew
 cp -pR /home/linuxbrew $HOME/
 
 # brew install ruby
@@ -32,5 +33,5 @@ ln -s ~/.cloudshell/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.cloudshell/Dotfiles/.tmuxinator ~/.tmuxinator
 
 # echo -e "\n Restart Cloud Shell for changes to take effect"
-source ~/.bashrc
+source $HOME/.bashrc
 
